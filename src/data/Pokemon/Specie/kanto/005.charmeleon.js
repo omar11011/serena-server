257773819688c738793c8524ee71d36013119f47
omar11011 <atarama11011@gmail.com>
2023-11-11 23:14:03 -0500

    Revisión de la morza

--- src/data/Pokemon/Specie/kanto/005.charmeleon.js
+++ src/data/Pokemon/Specie/kanto/005.charmeleon.js
@@ -1,20 +1,12 @@
+const base = JSON.parse(JSON.stringify(require("./004.charmander")))
+
 module.exports = {
+  ...base,
     pokedex: 5,
     keys: ["charmeleon"],
     name: "Charmeleon",
-    category: "Llama",
     height: 1.1,
     weight: 19.0,
-    egg_group: ["monstruo", "dragon"],
-    gender: {
-      male: 87.5,
-      female: 12.5
-    },
-    habitat: "montaña",
-    increase: "parabolico",
-    catch_ratio: 45,
-    base_friendship: 50,
-    hatching_steps: [5120, 5376],
     image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png"
   }
   
