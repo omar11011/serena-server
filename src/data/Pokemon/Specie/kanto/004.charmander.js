4bbd092af80b6eab40d5e865a03fa8575236fc5a
ArielB147 <95312814+ArielB147@users.noreply.github.com>
2023-11-12 00:12:17 -0400

    Cambios #1

---
+++ src/data/Pokemon/Specie/kanto/004.charmander.js
@@ -0,0 +1,20 @@
+module.exports = {
+    pokedex: 4,
+    keys: ["charmander"],
+    name: "Charmander",
+    category: "Lagartija",
+    height: 0.6,
+    weight: 8.5,
+    egg_group: ["monstruo", "dragon"],
+    gender: {
+      male: 87.5,
+      female: 12.5
+    },
+    habitat: "montaña",
+    increase: "parabolico",
+    catch_ratio: 45,
+    base_friendship: 50,
+    hatching_steps: [5120, 5376],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
+  }
+  
---
+++ src/data/Pokemon/Specie/kanto/005.charmeleon.js
@@ -0,0 +1,20 @@
+module.exports = {
+    pokedex: 5,
+    keys: ["charmeleon"],
+    name: "Charmeleon",
+    category: "Llama",
+    height: 1.1,
+    weight: 19.0,
+    egg_group: ["monstruo", "dragon"],
+    gender: {
+      male: 87.5,
+      female: 12.5
+    },
+    habitat: "montaña",
+    increase: "parabolico",
+    catch_ratio: 45,
+    base_friendship: 50,
+    hatching_steps: [5120, 5376],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png"
+  }
+  
