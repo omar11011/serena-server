f29c40ed5ee8051a0616c228bff0e648268023bf
ArielB147 <95312814+ArielB147@users.noreply.github.com>
2023-11-12 02:51:30 -0400

    Guardado 1

---
+++ src/data/Pokemon/Specie/0001-0050/017.pidgeotto.js
@@ -0,0 +1,20 @@
+module.exports = {
+    pokedex: 17,
+    keys: ["pidgeotto"],
+    name: "Pidgeotto",
+    category: "Pájaro",
+    height: 1.1,
+    weight: 30.0,
+    egg_group: ["volador"],
+    gender: {
+      male: 50,
+      female: 50
+    },
+    habitat: "bosque",
+    increase: "lento",
+    catch_ratio: 120,
+    base_friendship: 70,
+    hatching_steps: [3840, 4096],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png"
+  }
+  
