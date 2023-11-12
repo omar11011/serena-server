302cf2b192d3788f9900fd69391cb7d14e287744
omar11011 <atarama11011@gmail.com>
2023-11-12 00:32:25 -0500

    Edit pokemon 101-151

--- src/data/Pokemon/Specie/0126-0151/139.omastar.js
+++ src/data/Pokemon/Specie/0126-0151/139.omastar.js
@@ -1,19 +1,12 @@
+const base = JSON.parse(JSON.stringify(require("./138.omanyte")))
+
 module.exports = {
+    ...base,
     pokedex: 139,
     keys: ["omastar"],
     name: "Omastar",
     category: "Espiral",
     height: 1.0,
     weight: 35.0,
-    egg_group: ["agua-1", "agua-2"],
-    gender: {
-        male: 87.5,
-        female: 12.5,
-    },
-    habitat: "mar",
-    increase: "lento",
-    catch_ratio: 45,
-    base_friendship: 70,
-    hatching_steps: [7680, 7936],
     image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/139.png",
 }
